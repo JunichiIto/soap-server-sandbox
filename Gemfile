4ec0f6468f8d30ed312bfc170fd3d1d0c9bff8fd
Junichi Ito <jit@sonicgarden.jp>
2015-10-02 16:48:30 +0900

    Setup server

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ group :development do
   gem 'spring'
 end
 
+gem 'wash_out'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  wash_out :rumbas
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
